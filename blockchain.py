e000287c2861027eae923b26d0fcedd92a29d46e
Greg Larsen <gslarsen2718@gmail.com>
2025-07-10 17:13:34 -0400

    add match-case statement and logic for user choices

--- blockchain.py
+++ blockchain.py
@@ -2,22 +2,45 @@ blockchain = []
 
 
 def get_last_blockchain_value():
-    return blockchain[-1]
+    return blockchain[-1] if len(blockchain) > 0 else None
 
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction=[1]):
+    """Adds a new value to the blockchain, as well as the last transaction.
+    Args:
+        transaction_amount: The amount to be added.
+        last_transaction: The last transaction in the blockchain (default [1])."""
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
-tx_amount = 0
-
-while tx_amount != "x":
-    tx_amount = input("Your transaction amount (enter x to exit): ").lower()
-    if tx_amount == "x":
-        break
-    tx_amount = float(tx_amount)
-    last_transaction = get_last_blockchain_value() if len(blockchain) > 0 else [1]
-    add_value(last_transaction=last_transaction, transaction_amount=tx_amount)
+def get_transaction_value():
+    tx_amount = float(input("Your transaction amount: "))
+    return tx_amount
 
 
-print(blockchain)
+while True:
+    print("\nPlease choose:")
+    print("\t1: Add a new transaction value")
+    print("\t2: Print the blockchain blocks")
+    print("\tq: Quit the program")
+    user_choice = input("Your choice (1 or 2, enter q to exit): ").lower()
+
+    match user_choice:
+        case "q":
+            print("Quitting program...")
+            break
+        case "1":
+            tx_amount = get_transaction_value()
+            last_transaction = get_last_blockchain_value()
+            add_transaction(tx_amount, last_transaction)
+        case "2":
+            if len(blockchain) == 0:
+                print("no blocks yet!")
+            else:
+                print("The blocks in the blockchain are:")
+                for block in blockchain:
+                    print(block)
+        case _:  # Default case (like 'default' in other languages)
+            print("\nInvalid choice!", end=" ")
