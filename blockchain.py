4ed99b0f0cde1ee2d1fda176e895540a9d47baf5
Greg Larsen <gslarsen2718@gmail.com>
2025-07-29 15:54:17 -0400

    add get_balance for participants

--- blockchain.py
+++ blockchain.py
@@ -6,6 +6,7 @@ genesis_block = {
 blockchain = [genesis_block]
 open_transactions = []
 owner = "GregoryLarsen"
+participants = {owner}
 
 
 def hash_block(block):
@@ -16,11 +17,44 @@ def hash_block(block):
     return "-".join([str(val) for val in block.values()])
 
 
+def get_balance(participant):
+    """Calculates and returns the balance of a participant.
+    Args:
+        participant: The participant whose balance is to be calculated.
+    """
+    tx_sender = [
+        tx["amount"]
+        for block in blockchain
+        for tx in block["transactions"]
+        if tx["sender"] == participant
+    ]
+    open_tx_sender = [
+        tx["amount"] for tx in open_transactions if tx["sender"] == participant
+    ]
+    tx_sender.extend(open_tx_sender)
+    amount_sent = sum(tx_sender)
+
+    tx_recipient = [
+        tx["amount"]
+        for block in blockchain
+        for tx in block["transactions"]
+        if tx["recipient"] == participant
+    ]
+    open_tx_recipient = [
+        tx["amount"] for tx in open_transactions if tx["recipient"] == participant
+    ]
+    tx_recipient.extend(open_tx_recipient)
+    amount_received = sum(tx_recipient)
+
+    return amount_received - amount_sent
+
+
 def get_transaction_value():
     """Returns the transaction amount from the user input."""
+    tx_sender = input("Your name: ")
     tx_recipient = input("Recipient name: ")
     tx_amount = float(input("Your transaction amount: "))
-    return tx_recipient, tx_amount
+    return tx_sender, tx_recipient, tx_amount
 
 
 def add_transaction(recipient, sender=owner, amount=1.0):
@@ -32,6 +66,7 @@ def add_transaction(recipient, sender=owner, amount=1.0):
     open_transactions.append(
         {"sender": sender, "recipient": recipient, "amount": amount}
     )
+    participants.update([sender, recipient])
 
 
 def mine_block():
@@ -74,6 +109,7 @@ while waiting_for_input:
     print("\t1: Add a new transaction value")
     print("\t2: Mine a new block")
     print("\t3: Print the blockchain blocks")
+    print("\t4: Print participants")
     print("\th: Change the last transaction value")
     print("\tq: Quit the program")
     user_choice = input("Your choice: ").lower()
@@ -81,8 +117,9 @@ while waiting_for_input:
     match user_choice:
         case "1":
             tx_data = get_transaction_value()
-            recipient, amount = tx_data
-            add_transaction(recipient, amount=amount)
+            print(f"Transaction data: {tx_data}")
+            sender, recipient, amount = tx_data
+            add_transaction(recipient=recipient, sender=sender, amount=amount)
             print(f"open_transactions:\n{open_transactions}")
         case "2":
             mined_block = mine_block()
@@ -98,6 +135,13 @@ while waiting_for_input:
                 print("The blocks in the blockchain are:")
                 for block in blockchain:
                     print(block)
+        case "4":
+            if len(participants) == 0:
+                print("No participants yet!")
+            else:
+                print("The participants are:")
+                for participant in participants:
+                    print(participant)
         case "h":
             # h for hacking the first block - this is just an example
             if len(blockchain) > 0:
@@ -117,3 +161,4 @@ while waiting_for_input:
     if not verify_chain():
         print("Blockchain is invalid! Exiting...")
         break
+    print(f"Balance of {owner}: {get_balance(owner)}")
